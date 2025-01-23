128ac97106519c57fce6d733e9d974871a123220
heilotiitsmaa <heilo.tiitsmaa@voco.ee>
2025-01-23 15:25:47 +0200

    ül.3 komponendi sisustamine data'ga

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,27 @@
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 const App = () => {
+  const expenses = [
+    {
+    date: new Date(2025, 1, 23),
+    title: 'Sokolaadi jäätisekokteil',
+    price: 2.75
+  },
+  {
+    date: new Date(2025, 2, 16),
+    title: 'Maasika jäätisekokteil',
+    price: 3.29
+  },
+  {
+    date: new Date(2025, 7, 7),
+    title: 'Banaanijäätisekokteil',
+    price: 1.95
+  }
+  ]
   return (
     <div className="App">
-      <ExpenseItem />
+      <ExpenseItem data={expenses[0]} />
+      <ExpenseItem data={expenses[1]} />
     </div>
   );
 }
