a2cc5b872684e3bcbeed230b842f8dae74969b23
heilotiitsmaa <heilo.tiitsmaa@voco.ee>
2025-01-31 17:17:33 +0200

    8. ümbritseva komponendi rakendamine (teistes komponentides)

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,18 +1,19 @@
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
+import Card from './Card.jsx'
 const ExpenseItem = (prop) => {
+
 console.log(prop)
-    return (
-    <div className='expense-item'>
+
+return (
+    <Card className="expense-item">
         <ExpenseDate date={prop.data.date}/>
         <div className='expense-item__description'>
             <h2>{prop.data.title}</h2>
             <div className='expense-item__price'>{prop.data.price}</div>
         </div>
-    </div>
-
-    
-    )
+    </Card>
+  )
 }
 
 export default ExpenseItem //fail kättesaadavaks väljaspool
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable react/prop-types */
 //expense-item__price fail kättesaadavaks väljaspool
 import ExpenseItem from "./ExpenseItem.jsx";
-import Card from "./Card.jsx";
 import './Expenses.css'
+import Card from "./Card.jsx";
+
 const Expenses = (props) => {
     // Kontrollime saabunud props-e, et näha, mis andmed komponendile jõuavad.
   
     return (
       <Card className="expenses">
           <ExpenseItem data={props.expenses[0]}/>
-          <ExpenseItem data={props.expenses[1]}/>
+          <ExpenseItem data={props.expenses[2]}/>
       </Card>
     )
   };
