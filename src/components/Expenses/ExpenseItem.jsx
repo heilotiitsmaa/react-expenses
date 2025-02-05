aca1bce58970c3148590f9760f4c03d877e567bb
heilotiitsmaa <heilo.tiitsmaa@voco.ee>
2025-02-05 12:55:20 +0200

    Press here! 10. sündmuse töötlus komponentides

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -3,21 +3,21 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card.jsx'
 import {useState} from 'react';
 const ExpenseItem = (prop) => {
-    const [title, setTitle] = useState(prop.expenseData.title)
-
+    const [title, setTitle] = useState(prop.data.title) //title uueneb kogu aeg
+console.log(prop.data.title);
 const clickHandler = () => {
     console.log('Pressed it!')
-    setTitle(`Updated by click ${title}`)
+    setTitle(`Updated by click ${prop.data.title}`) //title jääb samaks
     console.log(title)
 }
 
 return (
     <Card className="expense-item">
-        <ExpenseDate date={prop.expenseData.date}/>
+        <ExpenseDate date={prop.data.date}/>
         <div className='expense-item__description'>
-            <h2>{prop.expenseData.title}</h2>
+            <h2>{prop.data.title}</h2>
             <h2>{title}</h2>
-            <div className='expense-item__price'>{prop.expenseData.price}</div>
+            <div className='expense-item__price'>{prop.data.price}</div>
         </div>
         <button onClick={clickHandler}>Press here!</button>
     </Card>
